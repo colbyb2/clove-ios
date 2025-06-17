69566c7d87c7af212b16de29edde2a978f713de2
colbyb2 <csbrown10@gmail.com>
2025-06-17 12:19:10 -0500

    Initial Commit

---
+++ Clove/App/CloveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloveApp.swift
+//  Clove
+//
+//  Created by Colby Brown on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CloveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clove/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clove
+//
+//  Created by Colby Brown on 6/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Test from Cursor!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
